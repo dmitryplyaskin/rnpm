99d0d9ed96a0ef7e840fed3b7ceb82ac8ee6dffe
Dimitry <dima@dima.dima>
2022-01-18 23:12:26 +0300

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Угадай число!");
+    println!("Введи свое число!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("WTF");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Твое число: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Меньше!"),
+            Ordering::Greater => println!("Больше!"),
+            Ordering::Equal => {
+                println!("С победкой!");
+                break;
+            }
+        }
+    }
 }
